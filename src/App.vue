b5c4e433970ba02b8e63d4a15c5c1bc0679c2599
Chaneice Jackson <c.jackson0126@gmail.com>
2023-06-20 21:43:30 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import ContactForm from './components/ContactForm.vue'
+import ContactList from './components/ContactList.vue'
+import sampleData from './sampleData'
+
+const contacts = ref(sampleData)
+
+const addContact = (contact) => {
+  contacts.value = [...contacts.value, contact]
+}
+</script>
+
+<template>
+  <h1 class="is-size-1 has-text-centered has-text-info">Vue Phonebook</h1>
+  <div class="container px-4">
+    <div class="columns">
+      <div class="column">
+        <ContactForm @submit="addContact" />
+      </div>
+      <div class="column">
+        <ContactList :contacts="contacts" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
